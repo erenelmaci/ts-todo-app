e6c9e634c778398d1acb0ddc73aa9ee5c5d92532
Eren Elmacı <erenx1623@gmail.com>
2023-04-11 18:27:56 +0300

    commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,4 @@
-import React,{FC} from 'react'
 import TodoListItem from './TodoListItem'
-
-
-//React.FC<IToDoList> yani diyoruz ki bu bir reactFunctional componenttir props oalrak da TodoList interfaceine uygun olarak props gönderebilir hale getirdik.Props alan componentlerde React.FC yi belirtmemiz zorunlu
-
-
 
 const TodoList:React.FC<ITodoList>= ({todos,toggleTodo,deleteTodo}) => {
 
